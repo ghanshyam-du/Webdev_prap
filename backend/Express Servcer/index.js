f75a7fae396f5b71a09c06965bb37eea1e2585dc
ghanshyam-du <ghanshyamd816@gmail.com>
2025-07-17 11:22:45 +0530

    udate

--- backend/Express Servcer/index.js
+++ backend/Express Servcer/index.js
@@ -1,6 +1,17 @@
 import express from "express";
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hi i am here!, Nice to meet u.");
+})
+app.get("/contact",(req,res)=>{
+    res.send("Hi this is the contact page and my contace number is : 90995583265")
+})
+app.get("/home",(req,res)=>{
+    res.send("Hi this is home page and wellcome")
+})
+
 app.listen(port, ()=>{
     console.log(`Server is running on the port no ${port}`)
 })
