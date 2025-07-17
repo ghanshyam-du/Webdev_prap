2823bbdb721ccf3877806352b23844f3c69f495d
ghanshyam-du <ghanshyamd816@gmail.com>
2025-07-18 02:06:06 +0530

    update

--- backend/Express Servcer/index.js
+++ backend/Express Servcer/index.js
@@ -12,6 +12,22 @@ app.get("/home",(req,res)=>{
     res.send("Hi this is home page and wellcome")
 })
 
+app.post("/test",(req,res)=>{
+    res.sendStatus(201);
+})
+
+
+app.put("/user/gd",(req,res)=>{
+    res.sendStatus(200);
+})
+
+
+app.patch("/user/gd",(req,res)=>{
+    res.sendStatus(200);
+})
+app.delete("/user/gd",(req,res)=>{
+    res.sendStatus(200);
+})
 app.listen(port, ()=>{
     console.log(`Server is running on the port no ${port}`)
 })
