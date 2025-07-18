06d779244bed0ec88b7aaefdcc198cdb2ebb5260
ghanshyam-du <ghanshyamd816@gmail.com>
2025-07-18 22:57:59 +0530

    update

--- 3.4+Middleware/3.4 Middleware/index1.js
+++ 3.4+Middleware/3.4 Middleware/index1.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 app.post("/submit",(req,res)=>{
   console.log(req.body);
-   res.send("Band name received!");
+   res.send("Band name received");
 })
 
 app.listen(port, () => {
