280c35f00b32c2f746b2c623e29212f741c01baa
ghanshyam-du <ghanshyamd816@gmail.com>
2025-09-13 22:17:49 +0530

    updated

--- auth/connection.js
+++ auth/connection.js
@@ -4,7 +4,7 @@ async function connectMongoDB() {
 
     return mongoose
 .connect("mongodb+srv://ghanshyamd5402_db_user:saxHtNAyMqyni1zT@cluster0.on4v9nr.mongodb.net/Practice?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=> console.log("MongoDB Connected"))
+.then(()=> console.log("MongoDB Connected!"))
 .catch((err)=> console.log("Mongo Error", err));
     
 }
